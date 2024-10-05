a55eb04a89c99e318ac650cfa838821ef453f264
CHRISKINUNGI <chrskinungi@gmail.com>
2024-10-05 18:33:06 +0300

    refactor: Update dependencies and remove unused files

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ dotenv.config(); // This must be called before accessing any env variables
 // Import the routes
 const authRoutes = require("./routes/auth");
 const visaApplicationRoutes = require("./routes/visaApplication");
-const adminRoutes = require("./routes/admin");
 const studentRoutes = require("./routes/student");
 const { protect, roleAuth } = require("./middlewares/auth");
 
